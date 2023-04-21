f6240e6c96c77fb9db80129b181c7c552ab8a4db
Rafath-Auvee <rbza98@outlook.com>
2023-04-22 00:50:33 +0600

    configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="text-red-500 text-5xl">Hello World</div>
-  )
+  return <div className="text-red-500 text-5xl">Hello World</div>;
 }
