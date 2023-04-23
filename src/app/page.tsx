85e0d9eeaf7d73926b5d1acfebada69869bacda6
Rafath-Auvee <rbza98@outlook.com>
2023-04-23 23:45:21 +0600

    redis setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <div className="text-red-500 text-5xl">Hello World</div>;
