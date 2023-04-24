7113018180667fed88097dcd782050da77850462
Rafath-Auvee <rbza98@outlook.com>
2023-04-25 02:21:57 +0600

    redis interface added

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -10,11 +10,14 @@ import { Redis, Requester } from '@upstash/redis'
 interface CustomRequester extends Requester {
   url?: string
   token?: string
+  request(options: any): any // add the required request property
 }
 
 const requester: CustomRequester = {
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  request: (options: any) => {}, // implement the required request function
 }
 
 export const db = new Redis(requester)
+
