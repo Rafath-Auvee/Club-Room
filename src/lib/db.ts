3e92ee03118439932fa23ab83d0ca7de05e600d3
Rafath-Auvee <rbza98@outlook.com>
2023-04-25 02:12:23 +0600

    redis requester added

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,15 @@
-import { Redis } from '@upstash/redis'
+// import { Redis } from '@upstash/redis'
 
-export const db = new Redis({
+// export const db = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// })
+
+import { Redis, Requester } from '@upstash/redis'
+
+const requester: Requester = {
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+}
+
+export const db = new Redis(requester)
